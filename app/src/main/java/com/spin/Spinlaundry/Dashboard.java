cd22006742f2355dbc93739d032ab0581e1fc0b9
glifordosade <glifordosade@gmail.com>
2024-09-30 23:31:10 +0700

    Praktikum 2
    Laporan Praktukum 2. membuat logika login dan membuat alur dari setiap menu

--- app/src/main/java/com/spin/Spinlaundry/Dashboard.java
+++ app/src/main/java/com/spin/Spinlaundry/Dashboard.java
@@ -1,16 +1,20 @@
 package com.spin.Spinlaundry;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import com.spin.laundry.R;
 
 public class Dashboard extends AppCompatActivity {
+
+    CardView Laundry, Layanan, Pelanggan, Promo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,44 @@ public class Dashboard extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Laundry = (CardView) findViewById(R.id.Laundry);
+        Layanan = (CardView) findViewById(R.id.Layanan);
+        Pelanggan = (CardView) findViewById(R.id.Pelanggan);
+        Promo = (CardView) findViewById(R.id.Promo);
+
+        Laundry.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Dashboard.this, LaundryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Pelanggan.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Dashboard.this, PelangganActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Layanan.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Dashboard.this, LayananActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Promo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Dashboard.this, PromoActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/spin/Spinlaundry/LoginActivity.java
+++ app/src/main/java/com/spin/Spinlaundry/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.spin.Spinlaundry;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +17,12 @@ import com.spin.laundry.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    String user = "saya";
+    String pass = "nama";
+
+    EditText Username,IDPassword;
+    Button btnlog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +32,29 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Username = (EditText)findViewById(R.id.IDUsername) ;
+        IDPassword = (EditText)findViewById(R.id.IDPassword) ;
+        btnlog = (Button)findViewById(R.id.btnlog);
+
+        btnlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = Username.getText().toString();
+                String password = IDPassword.getText().toString();
+
+                // Periksa apakah username dan password sesuai
+                if(username.equals(user) && password.equals(pass)) {
+                    // Jika benar, arahkan ke halaman Dashboard
+                    Intent intent = new Intent(LoginActivity.this, Dashboard.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // Jika salah, tampilkan pesan dan tetap di halaman login
+                    Toast.makeText(LoginActivity.this, "Username atau password salah", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
